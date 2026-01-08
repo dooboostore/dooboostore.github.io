7e15577d5ffc57deb8371dc4529697074dac14df
dooboostore <dooboostore@gmail.com>
2026-01-08 17:23:03 +0900

    page

--- source/apps/dooboostore-page/youtube/index.ts
+++ source/apps/dooboostore-page/youtube/index.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import process from 'process';
 
 // YouTube Video ID를 여기서 변경하세요
-const YOUTUBE_VIDEO_ID = '8i-ys9faa74';
+const YOUTUBE_VIDEO_ID = 'guRkQDSfiKk';
 
 // 번역 방식 선택: 'youtube' (YouTube 자동번역) 또는 'papago' (Papago API 번역)
 const TRANSLATION_METHOD: 'youtube' | 'papago' = 'youtube';
